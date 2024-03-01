0fc630c92b2e29e072e8c84eb28dde76f73373d3
bipulgyawali <bipulgyawali@bipuls-air>
2024-03-01 00:36:38 -0500

    assignment 1

---
+++ LSP_Project_Assignments_SP24/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * Bipul Gyawali
+ */
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
+ 
